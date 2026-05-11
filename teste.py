2ea95626782c653fb44c529f1db845de08e53cfb
DinaldoJorge <45379006+DinaldoJorge@users.noreply.github.com>
2026-05-11 11:42:51 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.image("NIKE.png")
 st.write("NOME DINALDO JORGE")
 st.image("DINO.png")
-st.write("DINALDO 49 ANOS FORMADO EM TELECO")
+st.write("DINALDO 49 ANOS FORMADO NO CURSO TELECO")
 st.image("ZA.png")
 st.link_button("Acessar", "https://wa.me/558399823445")
