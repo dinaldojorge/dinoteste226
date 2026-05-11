196711991315577055f738570ad9f98dd7bf7c94
DinaldoJorge <45379006+DinaldoJorge@users.noreply.github.com>
2026-05-11 13:33:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,11 @@
 import streamlit as st
-st.image("NIKE.png", width=300)
+
+# centralizar imagem e tamanho
+col1, col2, col3 = st.columns([1,2,1])
+with col2:
+    st.image("NIKE.png", width=300)
+
+
 #st.image("NIKE.png")
 st.write("NOME DINALDO JORGE")
 st.image("DINO.png")
